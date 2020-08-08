f4563ba755a106e6263b0f98c18fe53856355bbe
specilaist <specilaist@msn.com>
2020-08-08 13:24:40 -0700

    Question appears after test buttom, CSS is linked

--- quiz.js
+++ quiz.js
@@ -1,6 +1,3 @@
-const $instructions = document.querySelector('#instructions');
-const $questions = document.createElement('<div>');
-
 var questions = [
       {
             question: 'What is the color of the sky?', 
@@ -8,39 +5,75 @@ var questions = [
             choice: ['the sky is red', 'the sky is blue', 'the sky is green', 'the sky is brown']
       },
       {
-            question: 'What is the color of the sky?', 
-            answers: 'the sky is blue',
-            choice: ['the sky is red', 'the sky is blue', 'the sky is green', 'the sky is brown']
+            question: 'What sound does a dog make?', 
+            answers: 'Woof',
+            choice: ['Quack', 'Moo', 'Roar', 'Woof']
       },
       {
-            question: 'What is the color of the sky?', 
-            answers: 'the sky is blue',
-            choice: ['the sky is red', 'the sky is blue', 'the sky is green', 'the sky is brown']
+            question: 'How many apples is a dozen?', 
+            answers: '12',
+            choice: ['8', '12', '4', '9']
       },
       {
-            question: 'What is the color of the sky?', 
-            answers: 'the sky is blue',
-            choice: ['the sky is red', 'the sky is blue', 'the sky is green', 'the sky is brown']
+            question: 'What is the color of milk?', 
+            answers: 'White',
+            choice: ['Purple', 'White', 'Orange', 'Red']
       },
       {
-            question: 'What is the color of the sky?', 
-            answers: 'the sky is blue',
-            choice: ['the sky is red', 'the sky is blue', 'the sky is green', 'the sky is brown']
+            question: 'What type of shape is a tire?', 
+            answers: 'Circle',
+            choice: ['Circle', 'Square', 'Triangle', 'Hexagon']
       },
       {
-            question: 'What is the color of the sky?', 
-            answers: 'the sky is blue',
-            choice: ['the sky is red', 'the sky is blue', 'the sky is green', 'the sky is brown']
+            question: 'What do people drink?', 
+            answers: 'Water',
+            choice: ['Sand', 'Air', 'Metal', 'Water']
       },
+];
 
-]
+
+const $submitButton = $('#submitButton')
+
+
 
 var right = 0;
 var wrong = 0;
 
-for (var i = 0; i < questions.length; i++) {
-      document.createElement('<div>').textContent = questions[i].question;
+function startQuiz() {
+      const testInstructions = alert('Please make sure to hit submit after each answer');
+      var output = [];
+      const $instructions = $('#instructions');
+      const $answers = $('#container');
+      for (var i = 0; i < questions.length; i++) {
+            $instructions.text('Choose an answer:');
+            testQuestion = questions[i].question;
+            console.log(testQuestion);
+            $('#container').addClass('.css');
+            $('#container').text(testQuestion);
+            for (var i = 0; i < 4; i++) {
+                  var answer = $('<div>');
+                  answer.addClass('.answerss');
+                  $answers.append(answer);
+
+            } return;
+
+            // $questions.append('#container');
+            // answers = [];  
+
+      }
+            // on click listener
+      
+      var submit = $('<button>').text('submit').addId
+      $instructions.append(submit)
+      
+
 }
 
 
-console.log(questions[0].question);
+// we want to target the questions object
+// we want the question key to be read
+// we want to create four choices with button attributes
+// if the test taker gets it wrong/right an alert pops up telling them
+// an alert tells them how many right/wrong they have
+// At the end of the test they receive a final grade
+// they can add their name and include it as a highscore
